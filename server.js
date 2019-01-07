957c8907693f1cd8e2b5edfb0a17f4748be66215
SNiemiec <slawekniemiec@gmail.com>
2019-01-07 01:53:33 +0100

    server, db, front connected

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex') 
+const knex = require('knex');
+
 
 const db = knex ({
     client: 'pg',
@@ -23,6 +25,155 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+
+
+//POST ROUTE FOR SIGN IN
+
+app.post('/signin', (req, res) => {
+
+
+    db.select('email', 'hash').from('login')
+    
+    .where('email', '=', req.body.email)
+    
+    .then(data =>{
+
+    const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+   
+
+   if ( isValid ) {
+    return db.select('*')
+    .from('users')
+    .where('email', '=', req.body.email)
+    .then(user => {console.log(user[0]); res.json(user[0])} )
+    .catch(err => res.status(400).json('unable to get user'))
+
+   } else {
+
+    res.status(400).json('wrong credentials')
+
+   }
+
+    })
+
+    .catch(err => res.status(400).json('wrong credentials'))
+
+})
+
+
+//POST ROUTE FOR REGISTER
+
+app.post('/register', (req, res) => {
+
+    const {email, name, password} = req.body;
+
+    const hash = bcrypt.hashSync(password);
+db.transaction(trx => {
+
+trx.insert({
+
+hash: hash,
+email: email
+
+})
+
+.into('login')
+.returning('email')
+.then(loginEmail => {
+
+    return trx('users')
+    .returning('*')
+    .insert({
+
+    name: name,
+    email: loginEmail[0],
+    entries: []
+    })
+    
+    .then(user=> {
+
+        res.json(user[0]);
+    })
+
+
+})
+
+.then(trx.commit)
+.catch(trx.rollback)
+
+})
+ 
+    .catch(err => res.status(404).json('Unable to register'))
+
+})
+
+
+//UPDATE MAIN ARRAY (SUBMIT AND SAVE AND CLOSE)
+
+app.post('/todos', (req, res) => {
+
+    const {id} = req.body; 
+    
+    //let found = false;
+
+ db('users').where('id', '=', id)
+.update( {
+
+entries: req.body.entries
+
+})
+
+.returning('entries')
+.then(entries => {
+
+res.json(entries);
+
+})
+
+
+.catch(err => res.status(400).json('unable to get entries'))
+
+
+        //if(!found) {
+
+            //res.status(404).json('not found');
+        //}
+
+
+} )
+
+
+
+//EXTRA FOR PROFILE
+
+app.get('/profile/:id', (req, res) => {
+
+    const {id} = req.params;
+    
+    let found = false;
+
+    database.users.forEach(user => {
+
+    
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+
+        } 
+
+    })
+
+        if(!found) {
+
+            res.status(404).json('not found');
+        }
+   
+})
+
+
+app.listen(3000)
+
+/*
 
 const database = {
 
@@ -52,198 +203,5 @@ const database = {
     ]
 
 }
-
-
-//GET ALL USERS FROM THE DATABASE
-
-app.get('/', (req, res) => {
-
-    res.send(database.users)
-
-})
-
-
-
-//POST ROUTE FOR SIGN IN
-
-app.post('/signin', (req, res) => {
-
-
-    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password)
-
-    {
-       
-        res.json(database.users[0].entries)
-    } 
-    else {
-        res.status(400).json('error logging in')
-    }
-
-})
-
-
-//POST ROUTE FOR REGISTER
-
-app.post('/register', (req, res) => {
-
-    const {email, name, password} = req.body;
-
-    db('users')
-    .returning('*')
-    .insert({
-
-    name: name,
-    email: email,
-    entries: []
-    })
-    
-    .then(user=> {
-
-        res.json(user[0]);
-    })
-
-    .catch(err => res.status(404).json('Unable to register'))
-
-})
-
-
-
-//POST ROUTE FOR REGISTER
-/*
-app.post('/register', (req, res) => {
-
-    const {email, name, password} = req.body;
-
-    database.users.push(
-
-        {
-            id: 123,
-            name: name,
-            email: email,
-            password: password,
-            entries: []
-        }
-
-    )
-
-    res.json('register worked')
-
-})
-
 */
-
-//UPDATE MAIN ARRAY (SUBMIT AND SAVE AND CLOSE)
-
-app.post('/todos', (req, res) => {
-
-    const {id} = req.body; //problem
-    
-    let found = false;
-
-db.where('id', '=', id)
-.update( {
-
-entries: req.body.entries
-
-
-})
-
-.returning('entries')
-.then()
-
-    /*
-    database.users.forEach(user => {
-
-    
-        if (user.id === id) {
-            found = true;
-            user.entries = req.body.entries
-            return res.json(user.entries);
-
-        } 
-
-    })
-
-    */
-
-        if(!found) {
-
-            res.status(404).json('not found');
-        }
-
-
-} )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//UPDATE MAIN ARRAY (SUBMIT AND SAVE AND CLOSE)
-/*
-app.post('/todos', (req, res) => {
-
-    const {id} = req.body;
-    
-    let found = false;
-
-    database.users.forEach(user => {
-
-    
-        if (user.id === id) {
-            found = true;
-            user.entries = req.body.entries
-            return res.json(user.entries);
-
-        } 
-
-    })
-
-        if(!found) {
-
-            res.status(404).json('not found');
-        }
-
-
-} )
-
-*/
-//EXTRA
-
-app.get('/profile/:id', (req, res) => {
-
-    const {id} = req.params;
-    
-    let found = false;
-
-    database.users.forEach(user => {
-
-    
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
-
-        } 
-
-    })
-
-        if(!found) {
-
-            res.status(404).json('not found');
-        }
-   
-})
-
-
-app.listen(3000)
 
